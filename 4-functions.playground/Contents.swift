2a0be4c63fb0354e0c993c93196caad5e304c09f
gülçin çetin <cgulcn777@gmail.com>
2024-09-26 20:17:37 +0300

    Initial Commit

---
+++ 4-functions.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func noArgumentsAndNoReturnValue(){
+    "I am perfection"
+}
+noArgumentsAndNoReturnValue()
+
+func plusTwo(value: Int){
+    let newValue = value + 2
+}
+plusTwo(value: 30)
+
